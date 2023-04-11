4d20376362e7ddae92b09901f8901d17cec54505
Oscar Pimentel <oscariopimentel21@gmail.com>
2023-04-11 22:43:23 +0000

    comment df

--- Reto5/employees.py
+++ Reto5/employees.py
@@ -6,21 +6,21 @@ import codecs
 import matplotlib.pyplot as plt
 
 
-# load a limit number of rows
-def load_dataframe(nrows):
-    df = pd.read_csv("employees.csv", nrows=nrows)
-    return df
+# # load a limit number of rows
+# def load_dataframe(nrows):
+#     df = pd.read_csv("employees.csv", nrows=nrows)
+#     return df
 
 
 
-# Function with cache that load all employees
-@st.cache
-def load_alldata():
-    df = pd.read_csv("employees.csv")
-    return df
+# # Function with cache that load all employees
+# @st.cache
+# def load_alldata():
+#     df = pd.read_csv("employees.csv")
+#     return df
 
-dfEmp = load_dataframe(500)
-dfEmpAll = load_dataAll()
+# dfEmp = load_dataframe(500)
+# dfEmpAll = load_dataAll()
 
 # crear title de la app web.
 st.title("Employee analysis")

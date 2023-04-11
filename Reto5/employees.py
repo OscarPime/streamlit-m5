2cbd3965519110941892ea67ebb44d4881a98ea7
Oscar Pimentel <oscariopimentel21@gmail.com>
2023-04-11 22:53:41 +0000

    compleat all graphs

--- Reto5/employees.py
+++ Reto5/employees.py
@@ -6,21 +6,19 @@ import codecs
 import matplotlib.pyplot as plt
 
 
-# # load a limit number of rows
-# def load_dataframe(nrows):
-#     df = pd.read_csv("employees.csv", nrows=nrows)
-#     return df
+# load a limit number of rows
+def load_dataframe(nrows):
+    df = pd.read_csv("employees.csv", nrows=nrows)
+    return df
 
+# Function with cache that load all employees
+@st.cache
+def load_alldata():
+    df = pd.read_csv("employees.csv")
+    return df
 
-
-# # Function with cache that load all employees
-# @st.cache
-# def load_alldata():
-#     df = pd.read_csv("employees.csv")
-#     return df
-
-# dfEmp = load_dataframe(500)
-# dfEmpAll = load_dataAll()
+dfEmp = load_dataframe(500)
+dfEmpAll = load_dataAll()
 
 # crear title de la app web.
 st.title("Employee analysis")
@@ -34,33 +32,33 @@ show_df = sidebar.checkbox("Show Data Frame?", value=True )
 # Graph Age
 st.markdown("___")
 fig, ax = plt.subplots()
-ax.hist(df_employeescomplete["Age"])
+ax.hist(dfEmpAll["Age"])
 ax.set_xlabel("Age")
 st.header("Age Histogram")
 st.pyplot(fig)
 
-# # Graph Unit
-# st.markdown("___")
-# fig2, ax2 = plt.subplots()
-# ax2.hist(df_employeescomplete["Unit"])
-# ax2.set_xlabel("Unit")
-# ax2.set_xticklabels(ax2.get_xticklabels(), rotation=90)
-# st.header("Unit Frecuency")
-# st.pyplot(fig2)
+# Graph Unit
+st.markdown("___")
+fig2, ax2 = plt.subplots()
+ax2.hist(dfEmpAll["Unit"])
+ax2.set_xlabel("Unit")
+ax2.set_xticklabels(ax2.get_xticklabels(), rotation=90)
+st.header("Unit Frecuency")
+st.pyplot(fig2)
 
-# # Graph Hometown 
-# st.markdown("___")
-# #Create new dataframe
-# df_empl_homtown = df_employeescomplete[['Hometown','Attrition_rate']].groupby('Hometown').sum()
-# #st.dataframe(df_empl_homtown)
-# fig3, ax3 = plt.subplots()
-# y_pos = df_empl_homtown.index
-# x_pos = df_empl_homtown['Attrition_rate']
-# ax3.barh(y_pos, x_pos)
-# ax3.set_ylabel("Hometown")
-# ax3.set_xlabel("Attrition rate")
-# st.header('Hometowns with more Attrition Rate')
-# st.pyplot(fig3)
+# Graph Hometown 
+st.markdown("___")
+#Create new dataframe
+df_empl_homtown = dfEmpAll[['Hometown','Attrition_rate']].groupby('Hometown').sum()
+#st.dataframe(df_empl_homtown)
+fig3, ax3 = plt.subplots()
+y_pos = df_empl_homtown.index
+x_pos = df_empl_homtown['Attrition_rate']
+ax3.barh(y_pos, x_pos)
+ax3.set_ylabel("Hometown")
+ax3.set_xlabel("Attrition rate")
+st.header('Hometowns with more Attrition Rate')
+st.pyplot(fig3)
 
 # # Graph Age vs Attrition Rate 
 # st.markdown("___")
